ad470bb3ce928f08d46e2c971e14f2952a1a41a3
Damien GILLES <pro@gillesdamien.com>
2025-05-06 16:51:33 +0200

    update 5mai

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const presenceEl = document.getElementById('presence');
-const nomsEl = document.getElementById('noms');
-
-presenceEl.addEventListener('change', updateLink);
-nomsEl.addEventListener('input', updateLink);
-
-function updateLink() {
-    const presence = presenceEl.value;
-    const noms = nomsEl.value;
-    const subject = `RSVP Mariage Sarah & Julien - ${presence}`;
-    const body = `Noms: ${noms}\nPrésence: ${presence}`;
-    const mailtoLink = `mailto:s.nehme.perso@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-    document.getElementById('mailto').setAttribute('href', mailtoLink);
-    console.log(mailtoLink);
-};
 
 const navlElements = [...document.getElementsByClassName('navl')];
 
